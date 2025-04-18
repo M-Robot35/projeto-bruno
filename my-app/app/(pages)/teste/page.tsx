eed0f321a295c0555da7d1d8ea46f9a1040cb370
Bruno <brunoco56@hotmail.com>
2025-04-18 20:35:21 -0300

    estudo e treino

--- my-app/app/(pages)/teste/page.tsx
+++ my-app/app/(pages)/teste/page.tsx
@@ -1,21 +1,92 @@
 'use client'
-import Link from "next/link"; 
+import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useActionState, useState } from "react";
 
+//Objetos typeScript
+type Pessoa =
+  {
+    nome?: string,
+    idade: number
+  }
+
+// variavel de estados
 const x = [
   'thiago',
-  'bruno'
+  'Pedro',
+  'Joao'
+
 ]
 
 // useState     useEffect  useActionState 
-export default function LandinPage(){
-  const [state, setstate]= useState('suco')
-  
+export default function LandinPage() {
+
+  const [suco, setSuco] = useState('suco de laranja')
+  const [contador, setContador] = useState(0)
+  const [input, setInput] = useState("Teste");
+
+  const adicionarNumero = () => {
+    setContador(contador + 1);
+  }
+
+  const MudarNome = () => {
+    setSuco("Mudou de nome");
+  }
+
+  function chamarTodos() {
+    MudarNome();
+    adicionarNumero();
+  }
+
+  const mudarArray = () => {
+    x.map((nomes) => {
+      return <p>{nomes}</p>
+    })
+  }
+  ///Objeto
+  let pessoa: Pessoa;
+
+  pessoa = {
+    nome: "Bruno",
+    idade: 37
+  };
+
   return (
     <section>
-      <h1>{state}</h1>
+      <div className="grid h-180 content-center space-x-18 gap-8  px-120 py-12 ">
+        <div>
+          <h5 className="text-fuchsia-950  font-bold">A variavel é : {suco}
+            <p />
+            contando:{contador}</h5>
+          <p />
+        </div>
+        <br />
+        <div className="content-center">
+          <input className="bg-amber-600" onChange={event => setInput(event.target.value)} value={input} />
+          <div> valor de input:
+            {input}
+          </div>
+        </div>
+        <p />
+        <div className="content-center">
+          <Button color="primary" onClick={chamarTodos}> Contador</Button>
+        </div>
+
+        <div>
+          <h3>Maps</h3>
+          {
+            x.map((nomes) => {
+              return <p>{nomes}</p>
+            })
+          }
+        </div>
+
+        <br />
+        <h3>Objeto</h3>
+        <div>{pessoa.nome}</div>
+        <div>{pessoa.idade}</div>
+      </div>
     </section>
   )
 }
