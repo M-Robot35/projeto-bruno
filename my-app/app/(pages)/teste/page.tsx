12da998ac4af6ed2babe60cbfe7f753c8af0694f
Bruno <brunoco56@hotmail.com>
2025-04-30 16:08:00 -0300

    mais ajustes

--- my-app/app/(pages)/teste/page.tsx
+++ my-app/app/(pages)/teste/page.tsx
@@ -5,6 +5,14 @@ import { useActionState, useState, useEffect } from "react"; // Estudando
 import { Textarea } from "@/components/ui/textarea";
 import { estados, buscaDadosPromptDoBOt } from "@/app/actions/testeAction";
 import { Switch } from "@/components/ui/switch"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
 
 import {
   Accordion,
@@ -39,33 +47,42 @@ const prompts = [
 
 export default function Page() {
 
-  const [dadosBot, setDadosBot] = useState<string | "">("");
+  //const [dadosBot, setDadosBot] = useState<string | "">("");
 
-  const [dadosBot1, setDadosBot1] = useState<typeof prompts>(prompts);
+  const [dadosBot, setDadosBot] = useState<typeof prompts>(prompts);
+
+  const [isEnabled, setIsEnabled] = useState(true);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+
 
   // Usando useEffect para buscar os dados de forma assíncrona
-  useEffect(() => {
-    const fetchData = async () => {
-      const retornoBot = await buscaDadosPromptDoBOt();
-      setDadosBot(retornoBot); // Atualiza o estado com os dados retornados
-    };
-    fetchData(); // Chama a função de buscar os dados quando o componente for montado
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const retornoBot = await buscaDadosPromptDoBOt();
+  //     setDadosBot(retornoBot); // Atualiza o estado com os dados retornados
+  //   };
+  //   fetchData(); // Chama a função de buscar os dados quando o componente for montado
+  // }, []);
 
-  async function chamaFuncaoIn(formData: FormData) {
-    await estados(null, formData); // Passando o estado anterior (prevState) e o formData
-    const retornoBot = await buscaDadosPromptDoBOt();
-    setDadosBot(retornoBot); // Atualiza o estado com os dados retornados    
-  }
+  // async function chamaFuncaoIn(formData: FormData) {
+  //   await estados(null, formData); // Passando o estado anterior (prevState) e o formData
+  //   const retornoBot = await buscaDadosPromptDoBOt();
+  //   setDadosBot(retornoBot); // Atualiza o estado com os dados retornados    
+  // }
 
   return (
     <section>
-
-      <div className="flex h-100 content-center space-x-18 gap-8 px-120 py-12 ">
+      <div className="flex h-100 content-center space-x-18 gap-8 px-120 py-12  ">
         <div className="w-full">
-
+          <div className="border-amber-400 items-end text-end gap-2 w-full h-15 ">         
+              <Switch id="ativarBot" className="gap-2 mt-2"
+                checked={isEnabled}
+                onCheckedChange={toggleSwitch}
+              /><label className="ml-3">Ativar/Desativar Robô</label>
+          </div>
           {
-            dadosBot1.map((item) => {
+            dadosBot.map((item) => {
               return (
                 <Accordion type="single" collapsible className="w-full">
                   <AccordionItem value="item-1">
@@ -73,17 +90,17 @@ export default function Page() {
                     <AccordionTrigger>{item.nome}</AccordionTrigger>
                     <AccordionContent>
 
-                      <Textarea
+                      <Textarea className="gap-2"
                         name="textAreaInput"
                         placeholder="Sua mensagem aqui"
-                       // onChange={event => setDadosBot(event.target.value)}
+                        // onChange={event => setDadosBot(event.target.value)}
                         // value={dadosBot1.find(Item => Item.status == true)?.prompt}
-                        value={item.prompt}                         
-                        />
-                         <Switch
-                      checked={item.status}
-                      //onCheckedChange={n}
-                    />
+                        value={item.prompt}
+                      />
+                      <Switch className="gap-2 mt-2"
+                        checked={item.status}
+                      //onCheckedChange={n}                    
+                      /><label className="ml-3">Ativar/Desativar Prompt</label>
                     </AccordionContent>
                   </AccordionItem>
                 </Accordion>
@@ -91,48 +108,7 @@ export default function Page() {
             })}
         </div>
       </div>
-      {/* {
-        dadosBot1.map((item) => {
-          return (
-            <div className="bg-amber-800 text-black">{item.nome}</div>
-          )
-        })
-      } */}
 
-{/* 
-      <div className="grid h-100 content-center space-x-18 gap-8 px-120 py-12">
-
-        <form action={chamaFuncaoIn}>
-
-          <Button type="button">
-            Enviar texto e resposta do bot
-          </Button>
-
-
-          <Textarea
-            name="textAreaInput"
-            placeholder="Sua mensagem aqui"
-            // onChange={event => setDadosBot(event.target.value)}
-            value={
-              dadosBot1.find(Item => Item.status == true)?.prompt
-            }
-          // value={dadosBot || ""}
-          />
-          <div className="pt-5" />
-
-          <Button type="submit">
-            Enviar texto e resposta do bot
-          </Button>
-
-        </form>
-        {
-          dadosBot1.map((item) => {
-            return (
-              <div className="bg-amber-800 text-black">{item.nome}</div>
-            )
-          })
-        }
-      </div> */}
     </section>
   );
 }
